c50c8944ef079a84d3bf8ae93a8198e2910cd1f8
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-01-25 15:12:06 +0200

    added salt generation

--- encrypt/encryption.py
+++ encrypt/encryption.py
@@ -4,3 +4,33 @@ data
 """
 
 __author__ = 'Johnny Ilanga'
+
+from Cryptodome.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad, unpad
+from string import ascii_letters
+from random import randint, choice
+import pickle
+from os.path import join as save_path
+
+def generating_salt_and_pepper():
+    """
+    Generates the salt and pepper. The salt is a randomly generated sequence of bytes needed
+    for the encryption process. Salt is Key
+    
+    Returns:
+        byte: Returning the salt
+        string: The pepper aka password
+    """
+    
+    recovery_key = dict()
+    
+    salt = get_random_bytes(32)
+    recovery_key["salt"] = salt
+    
+    # randomly generated password. length of password is randomly selected 
+    password = "".join([choice(ascii_letters) for i in range(randint(8,17))])
+    recovery_key["pepper"] = password
+    
+    return salt, password, recovery_key
