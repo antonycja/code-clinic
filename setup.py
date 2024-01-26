ae23aae56b66ade2edecb580fadc1c5263355047
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-01-26 17:14:04 +0200

    added folder creation function

--- setup.py
+++ setup.py
@@ -11,3 +11,18 @@ __author__ = 'Johnny Ilanga'
 __version_ = '1.0'
 
 
+def secure_folder():
+    """
+    creates the dir tree structure
+    
+    Returns:
+        dict : dir paths
+    """
+    
+    user_dir = files.get_home()
+    main_dir = files.create_dir(user_dir,'.elite') # main folder
+    auth_dir = files.create_dir(main_dir,'.access')  # user login details, authentication key and token
+    recon_dir = files.create_dir(main_dir,'.recon') # key recovery data
+    key_dir = files.create_dir(main_dir,'.elite')   # key
+    
+    return {"main":main_dir, "auth":auth_dir, "recon": recon_dir, "key":key_dir}
