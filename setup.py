aaecf513ff42340fbd09799f87df9e16518c3412
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-01-26 16:07:30 +0200

    setting setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+code clinic setup 
+"""
+
+from config import config
+from encrypt import encryption
+from file_handling import files
+from helpers import writer, animation
+
+__author__ = 'Johnny Ilanga'
+__version_ = '1.0'
+
+
