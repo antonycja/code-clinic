554bec8c84647cbccb5835292fe0f6c08733a66a
jilanga023 <jilanga023@student.wethinkcode.co.za>
2024-02-02 08:12:35 +0200

    made encrypting universal for both the creds and client secre

--- setup.py
+++ setup.py
@@ -31,60 +31,52 @@ def secure_folder():
     return {"main":main_dir, "auth":auth_dir, "recon": recon_dir, "key":key_dir}
 
 
-def encrypt_it(user_info: dict, dirs: dict):
+def encrypt_it(data: dict, dirs: dict, key_name: str, key_ext: str, recon_name: str, recon_ext: str, file_name: str, file_ext: str):
     """
-    Main Encryption method. Encrypts the user data and saves it onto your local machine
+        Main Encryption method. Encrypts the user data and saves it onto your local machine
 
     Args:
         user_info (dict): User configuration details
         dirs (dict): dict containing dir paths to store files
-    """   
-    
-    salt, pepper, recon = encryption.generating_salt_and_pepper()
-    
-    key = encryption.generate_key(salt,pepper)
-    encryption.save_key(dirs['key'],'.key','elite',key)
-    writer.capture_pickle(dirs['recon'],'.SOS','recon',recon)    
-    data = encryption.convert_str_to_bytes(user_info)
-    cipher, enc_data = encryption.encrypt_data(key,data)
-    encryption.write_enc_data(cipher,dirs["auth"],'.config','elite',enc_data)
-
-    return
-
-def encrypt_creds(cred_ref: dict, dirs: dict):
+        key_name (str): The name of the key file
+        key_ext (str): the extension of the key file
+        recon_name (str): the name of the recon file 
+        recon_ext (str): the extension of the recon file
+        file_name (str): the name of the file
+        file_ext (str): the extension of the file
     """
-    Main Encryption method for encrypting and writing the credentials in user dir
-    
-    Args:
-        cred_ref (dict): credentials source
-    """   
     
     salt, pepper, recon = encryption.generating_salt_and_pepper()
     
     key = encryption.generate_key(salt,pepper)
-    encryption.save_key(dirs['key'],'.key','cred',key)
-    writer.capture_pickle(dirs['recon'],'.SOS','cred',recon)    
-    data = encryption.convert_str_to_bytes(cred_ref)
-    cipher, enc_data = encryption.encrypt_data(key,data)
-    encryption.write_enc_data(cipher,dirs["auth"],'.credentials','cred',enc_data)
+    encryption.save_key(dirs['key'],f'.{key_name}',f'.{key_ext}',key)
+    writer.capture_pickle(dirs['recon'],f'.{recon_name}',f'.{recon_ext}',recon)    
+    data_to_enc = encryption.convert_str_to_bytes(data)
+    cipher, enc_data = encryption.encrypt_data(key,data_to_enc)
+    encryption.write_enc_data(cipher,dirs["auth"],f'.{file_name}',f'.{file_ext}',enc_data)
 
     return
 
 
 
-def decrypt_it(dirs: dict):
+def decrypt_it(dirs: dict,key_name: str, key_ext: str, file_name: str ,file_ext: str):
     """
-    Main Decrypting Method. Decrypts all the encrypted data
+        Main Decrypting Method. Decrypts all the encrypted data
 
     Args:
         dirs (dict): dict containing dir paths of encrypted files
-
+        key_name (str): the name of the file containing the key 
+        key_ext (str): the extension of the file (e.g .txt)
+        file_name (str): the name of then file we want to decrypt
+        file_ext (str): the extension of the file we want to decrypt
+        
     Returns:
-        dict : user_info (name, mail, pass)
+        dict : dictionary containing the data requested
     """
 
-    key = encryption.read_key(dirs["key"],'.key.elite')
-    data = encryption.read_enc_data(key,save_path(dirs["auth"],'.config.elite'))
+
+    key = encryption.read_key(dirs["key"],f'.{key_name}.{key_ext}')
+    data = encryption.read_enc_data(key,save_path(dirs["auth"],f'.{file_name}.{file_ext}'))
     clean_up = encryption.convert_bytes_to_str(data)
     user_info = encryption.data_ablution(clean_up)
 
