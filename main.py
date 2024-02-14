190915d0f126c371c3dba2483125d5bc0a722818
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-02-14 13:30:11 +0200

    Added user_input user input function gets input from the user to specify what data to get from the user.

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 from view_calendar.view_calendar import get_calendar_results
 
-get_calendar_results(0, 7)
+
+def get_user_input():
+    while True:
+        calendar = input("Which calendar would you like to view? \n(0). All Calendars.\n(1). Personal Calendar Only.\n(2). Code Clinics Calendar Only.\nEnter the number of the calendar here: ")
+        print()
+        if len(calendar) == 0:
+            break
+        if calendar.isdigit():
+            calendar = int(calendar)
+            break
+        
+    while True:
+        days = input(
+            "Enter the number of days you would like to view, leave empty for (default = 7).\nEnter a number here: ")
+        print()
+        if len(days) == 0:
+            days = 0
+            break
+        if days.isdigit():
+            days = int(days)
+            break
+    return calendar, days
+
+calendar, days = get_user_input()
+
+if days != 0:
+    get_calendar_results(calendar, days)
+else:
+    get_calendar_results(calendar)
+    
+# get_calendar_results(0, 7)
+
