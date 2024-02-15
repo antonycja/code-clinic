86483765db299103f23d6dfd5a910e75e710b3dd
cthomas023 <cthomas023@student.wethinkcode.co.za>
2024-02-15 23:12:00 +0200

    Added volunteer module and function to create open slots

---
+++ main.py
@@ -0,0 +1,15 @@
+from authentication.auth import *
+from volunteer.volunteer import *
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+creds = authenticate_user()
+calendar_id = "c_7f60d63097ebf921579ca266668826f490dc72478a9d37d17ad62046836f598a@group.calendar.google.com"
+
+try:
+    service = build('calendar', 'v3', credentials=creds)
+
+except HttpError as error:
+        print("An error occured:", error) 
+
+create_open_slots(service, calendar_id)
---
+++ volunteer/volunteer.py
@@ -0,0 +1,52 @@
+from google.oauth2.credentials import Credentials
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+from google.auth.transport.requests import Request
+
+from googleapiclient.discovery import build
+
+from datetime import datetime, timedelta
+
+
+def create_open_slots(service, calendar_id):
+    start_time = datetime(2023, 3, 1, 9, 0, 0) - timedelta(hours=2)  # Start time (Year, Month, Day, Hour, Minute, Second)
+
+    end_time = datetime(2023, 3, 1, 10, 0, 0)  - timedelta(hours=2) #1 End time (Year, Month, Day, Hour, Minute, Second)
+
+    interval = timedelta(minutes=30) 
+
+    for i in range(20):
+        event = {
+        'summary': 'Open Slot',
+        'location': 'WTC CPT',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2015-05-28T09:00:00-07:00',
+            'timeZone': 'Africa/Johannesburg',
+        },
+        'end': {
+            'dateTime': '2015-05-28T17:00:00-07:00',
+            'timeZone': 'Africa/Johannesburg',
+        },
+        'recurrence': [
+            'RRULE:FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR'
+        ],
+        'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+            {'method': 'popup', 'minutes': 10},
+            ],
+        },
+        }
+
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        print('Events created: %s' % (event.get('htmlLink')))
+        start_time += interval
+        end_time += interval
+
+
