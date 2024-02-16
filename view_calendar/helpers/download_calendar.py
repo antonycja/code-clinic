10c28cfec0061905eeacd68ca0e502ef239bb728
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-02-16 20:25:17 +0200

    added func to check if calendar chnaged since last download

--- view_calendar/helpers/download_calendar.py
+++ view_calendar/helpers/download_calendar.py
@@ -1,4 +1,15 @@
 import csv
+def calendar_data_changed(data_list, filename):
+    try:
+        with open(filename, "r") as cal_data:
+            csv_file = csv.reader(cal_data)
+            print(csv_file)
+            pass
+        pass
+        
+    except FileNotFoundError:
+        pass
+    pass
 
 def write_to_csv_file(data_list):
     fields = [name for name in data_list[0].keys()]
