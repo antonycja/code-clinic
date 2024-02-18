23d1d4b6728a06a546fe96d476ebfdac46c4dcdb
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-02-18 10:10:25 +0200

    convert csv to list of dict like the one used to create the csv

--- view_calendar/helpers/download_calendar.py
+++ view_calendar/helpers/download_calendar.py
@@ -3,7 +3,17 @@ def calendar_data_changed(data_list, filename):
     try:
         with open(filename, "r") as cal_data:
             csv_file = csv.reader(cal_data)
-            print(csv_file)
+            data_to_list = list(csv_file)
+            keys = data_to_list[0]
+            list_of_dict_data = [dict(zip(keys, data_list)) for data_list in data_to_list[1:]]
+            
+            for item in list_of_dict_data:
+                item['attendees'] = eval(item['attendees'])
+            
+            if data_list == list_of_dict_data:
+                return False
+            else:
+                return True
             pass
         pass
         
