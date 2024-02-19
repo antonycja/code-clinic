7c3a12328c5c453801d08956f5311dc540cd7741
jilanga023 <jilanga023@student.wethuinkcode.co.za>
2024-02-19 09:11:50 +0200

    edited login to make way for click

--- config/config.py
+++ config/config.py
@@ -10,51 +10,60 @@ __all__ = [
 ]
 
 
-# user_data operations    
-def generate_logIn_cred():
+# user_data operations
+def generate_logIn_cred(username: str = None, useremail: str = None, password:str = None ):
     """
-    Generates the user login credentials 
+    Generates the user login credentials
     Returns:
         tuple : username, useremail, password
     """
-    
+
     # initialization phase
     mail_pattern = '@student.wethinkcode.co.za'
     pattern = re.compile(r"^[a-zA-Z\s]+[0-9]{3}"+f"{mail_pattern}$")
     data = dict()
 
-    while True:
-        username = input('Please provide your username: ').lower().strip()
-        if re.match('^[a-zA-z]+[0-9]{3}$',username):
-            data["username"] = username
-            break
-        else:
-            print('Incorrect username format: (johndoe023).\n')
-            
 
-    while True:
+    if username == None or not re.match(r'^[a-zA-z]+[0-9]{3}$',username):
+        while True:
+            if not re.match(r'^[a-zA-z]+[0-9]{3}$',username) and username != None:
+                print('Incorrect username format: (johndoe023).')
 
-        useremail = input('Please provide your email: ').lower().strip()
-        if re.match(pattern,useremail):
-            data["email"] = useremail
-            break
-        else:
-            print('Incorrect email format: (johndoe023@student.wethinkcode.co.za)\n')
-            
-    
-    while True:
+            username = input('Please provide your username: ').lower().strip()
 
-        password = getpass('Please provide a passphrase: ')
-        confirm_password = getpass('Enter same passphrase: ')
-        
-        if len(password) == 0 and len(confirm_password) == 0:
-            data["password"] = None
-            break
-        
-        if password == confirm_password:
-            data["password"] = password
-            break
-        else:
-            print('Passphrases do not match. Try again.\n')
-    
+            if re.match('^[a-zA-z]+[0-9]{3}$',username):
+                data["username"] = username
+                break
+    else:
+        data["username"] = username
+
+    if useremail == None or not re.match(pattern,useremail):
+        while True:
+
+            if not re.match(pattern,useremail) and useremail != None:
+                print('Incorrect email format: (johndoe023@student.wethinkcode.co.za).')
+
+            useremail = input('Please provide your email: ').lower().strip()
+            if re.match(pattern,useremail):
+                data["email"] = useremail
+                break
+    else:
+        data["email"] = useremail
+
+    if password == None:
+        while True:
+
+            password = getpass('Please provide a passphrase: ')
+            confirm_password = getpass('Enter same passphrase: ')
+
+            if len(password) == 0 and len(confirm_password) == 0:
+                data["password"] = None
+                break
+
+            if password == confirm_password:
+                data["password"] = password
+                break
+            else:
+                print('Passphrases do not match. Try again.\n')
+
     return data
