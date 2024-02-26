feb3aa312b83912dea6d3fd20b6aca2461d70ea5
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-02-26 16:24:10 +0200

    added convert csv to dict function

--- view_calendar/helpers/download_calendar.py
+++ view_calendar/helpers/download_calendar.py
@@ -1,5 +1,20 @@
 import csv
-def calendar_data_changed(data_list:list, filename:str) -> bool:
+
+
+def convert_csv_to_dict(filename: str) -> list:
+    with open(filename, "r") as cal_data:
+        csv_file = csv.reader(cal_data)
+        data_to_list = list(csv_file)
+        keys = data_to_list[0]
+        list_of_dict_data = [dict(zip(keys, data_list))
+                             for data_list in data_to_list[1:]]
+
+        for item in list_of_dict_data:
+            item['attendees'] = eval(item['attendees'])
+    return list_of_dict_data
+
+
+def calendar_data_changed(data_list: list, filename: str) -> bool:
     """check whether the calendar data has changed compared to the data in the filename.
 
     Args:
