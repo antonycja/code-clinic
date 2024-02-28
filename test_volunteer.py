9c4c0b8cd029d1df06bc97c5f2526e6081c39d90
cthomas023 <cthomas023@student.wethinkcode.co.za>
2024-02-28 14:55:38 +0200

    removed test for get_event and added tests for create_volunteer, cancel_event and campus_abb

--- test_volunteer.py
+++ test_volunteer.py
@@ -17,7 +17,40 @@ class TestVolunteerFunctions(unittest.TestCase):
         service = build_service(creds)
         self.assertIsNotNone(service)
 
-class Test_Volunteer(unittest.TestCase):
-    
-    def test_get_event(self):
-        self.assertEqual(volunteer.get_event())
+
+    def test_create_volunteer_slot(self):
+        cancel_event(creds, self.starttime, self.endtime,self.volunteer_email, self.calendar_id)
+        message = create_volunteer_slot(creds, self.volunteer_email, self.starttime, self.endtime, self.campus, self.calendar_id)
+        self.assertEqual(message, "Slot successfully created.")
+
+
+    def test_create_volunteer_slot_already_booked(self):
+        message = create_volunteer_slot(creds, self.volunteer_email, self.starttime, self.endtime, self.campus, self.calendar_id)
+        message = create_volunteer_slot(creds, self.volunteer_email, self.starttime, self.endtime, self.campus, self.calendar_id)
+        self.assertEqual(message, "You are already attending a session at the specified time.")
+        cancel_event(creds, self.starttime, self.endtime,self.volunteer_email, self.calendar_id)
+
+
+    def test_cancel_event(self):
+        create_volunteer_slot(creds, self.volunteer_email, self.starttime, self.endtime, self.campus, self.calendar_id)
+        message = cancel_event(creds, self.starttime, self.endtime,self.volunteer_email, self.calendar_id)
+        self.assertEqual(message, 'Slot successfully cancelled.')
+
+
+    def test_cancel_event_not_attending(self):
+        create_volunteer_slot(creds, self.volunteer_email, self.starttime, self.endtime, self.campus, self.calendar_id)
+        self.volunteer_email = 'amaposa023@wethinkcode.student.co.za'
+        self.starttime = '2024-01-02T17:00:00+02:00'
+        self.endtime = '2024-01-02T17:30:00+02:00'
+        message = cancel_event(creds, self.starttime, self.endtime,self.volunteer_email, self.calendar_id)
+        self.assertEqual(message, 'You cannot cancel a slot that you are not attending.')
+
+
+    def test_campus_abb(self):
+        campus = 'Cape Town'
+        abb = campus_abb(campus)
+        self.assertEqual(abb, 'CPT')
+
+
+if __name__ == '__main__':
+    unittest.main()
