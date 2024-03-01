0bc5b21834aab78f6f788df76f966c7a1116bc22
cthomas023 <cthomas023@student.wethinkcode.co.za>
2024-03-01 14:58:35 +0200

    minor changes to cancel booking func

--- calendar_logic/volunteer.py
+++ calendar_logic/volunteer.py
@@ -79,7 +79,7 @@ def create_volunteer_slot(creds, volunteer_email, starttime, endtime, campus, ca
 
 def is_booked(starttime, endtime, email, service, calendar_id):
     """
-    Check if the volunteer is already booked for a session during the specified time.
+    Check if a volunteer has already booked a slot for the specified time.
 
     Args:
         starttime (str): Start time of the event in 'YYYY-MM-DDTHH:MM:SS' format.
@@ -89,7 +89,7 @@ def is_booked(starttime, endtime, email, service, calendar_id):
         calendar_id (str): ID of the Google Calendar.
 
     Returns:
-        bool: True if the volunteer is booked; False otherwise.
+        bool: True if the slot is booked; False otherwise.
     """
     event_id = get_event(service, calendar_id, starttime, endtime, email)
     if not event_id:
@@ -155,8 +155,13 @@ def cancel_event(creds, starttime, endtime, volunteer_email, calendar_id = calen
     try:
         event_id = get_event(service, calendar_id, starttime, endtime, volunteer_email)
         event = service.events().get(calendarId=calendar_id, eventId=event_id).execute()
-        if len(event['attendees']) > 1 or event['attendees'][0]['email'] != volunteer_email:
+
+        if len(event['attendees']) > 1:
             message = 'You cannot cancel a slot that has already been booked by a student.'
+        
+        elif event['attendees'][0]['email'] != volunteer_email:
+            message = 'you cannot cancel a slot that you have not volunteered for.'
+
         else:
             service.events().delete(calendarId=calendar_id, eventId=event_id).execute()
             message = 'Slot successfully cancelled.'
@@ -230,4 +235,3 @@ def campus_abb(campus: str):
 
     return campus
 
-# def store_volunteer_data(event_id, ):
