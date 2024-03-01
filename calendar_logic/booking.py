ebf9bfc899cebc260bd2578a7bd9ce205ed16224
jilanga023 <jilanga023@student.wethuinkcode.co.za>
2024-03-01 12:43:39 +0200

    bulela's updated booking.py

--- calendar_logic/booking.py
+++ calendar_logic/booking.py
@@ -1,16 +1,19 @@
 """
-This module is designed for booking functions
+The following module contains functions for booking
 """
 
 
 import datetime as dt
+from ics import Event, Calendar
+from datetime import timedelta
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 
-
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 CLINIC_CALENDAR_ID = "c_7f60d63097ebf921579ca266668826f490dc72478a9d37d17ad62046836f598a@group.calendar.google.com"
 YEAR = 2024
+USER_EMAIL = 'cthomas023@student.wethinkcode.co.za'
 months = {
     1 : 31,
     2 : 29,
@@ -27,8 +30,6 @@ months = {
 }
 
 
-
-
 def book_slot(creds, booking_info : dict, USER_EMAIL) -> tuple:
     """
     Books a slot specified by the user.
@@ -43,6 +44,11 @@ def book_slot(creds, booking_info : dict, USER_EMAIL) -> tuple:
 
     """
     try:
+        booking_info['dateTime'] = get_start_date_time(booking_info['dateTime'])
+        if 'invalid' in booking_info['dateTime']:
+            message =  booking_info['dateTime']
+            return False, message
+        
         service = build('calendar', 'v3', credentials=creds)
         success, message = update_event(service, booking_info, USER_EMAIL)
 
@@ -51,7 +57,6 @@ def book_slot(creds, booking_info : dict, USER_EMAIL) -> tuple:
 
     except HttpError as error:
         print("An error occured:", error) 
-
 
 
 def update_event(service, booking_info : dict, USER_EMAIL) -> tuple:
@@ -76,7 +81,7 @@ def update_event(service, booking_info : dict, USER_EMAIL) -> tuple:
         if event['start']['dateTime'] == booking_info['dateTime']:
             number_of_attendees = len(event['attendees'])
             if number_of_attendees == 2:
-                message = "Slot is already booked. TRY ANOTHER."
+                message = "Slot is already booked. Try another!"
                 return False, message
             elif is_volunteer(event, USER_EMAIL):
                 message = "You are a volunteer for this slot. You cannot book it!"
@@ -88,6 +93,7 @@ def update_event(service, booking_info : dict, USER_EMAIL) -> tuple:
 
             event_id = event['id']
 
+
             updated_event = service.events().update(calendarId= CLINIC_CALENDAR_ID, eventId=event_id, body=event).execute()
 
             message = "Booking Successful!"
@@ -95,8 +101,9 @@ def update_event(service, booking_info : dict, USER_EMAIL) -> tuple:
             return True, message
     
 
-    message = "There is no volunteer for the slot you selected. TRY ANOTHER."
+    message = "There is no volunteer for the slot you selected. Try another."
     return False, message
+
 
 def is_volunteer(event, USER_EMAIL) -> bool:
     return is_attendee(event, USER_EMAIL)
@@ -106,7 +113,6 @@ def is_attendee(event, USER_EMAIL):
     attendee = event['attendees'][0]
 
     return attendee['email'] == USER_EMAIL
-
 
 
 def create_event(service):
@@ -156,7 +162,7 @@ def cancel_booking(creds, booking_info, USER_EMAIL):
 
 
     except HttpError as err:
-        message = f"An error occurred: {err}"
+        message = f"An error occured: {err}"
         print(message)
 
 
@@ -172,6 +178,11 @@ def remove_attendee(service, booking_info : dict, USER_EMAIL):
     Returns:
         (success, message) (tuple): (var)success(boolean) is whether the removing was a success or not. (var)message(str) is the appropriate message to display.
     """
+    booking_info['dateTime'] = get_start_date_time(booking_info['dateTime'])
+    if 'invalid' in booking_info['dateTime']:
+        message = booking_info['dateTime']
+        return False, message
+    
     date_time = booking_info['dateTime']
 
     now = dt.datetime.utcnow().isoformat() + "Z"
@@ -192,10 +203,12 @@ def remove_attendee(service, booking_info : dict, USER_EMAIL):
 
             message = "Booking Cancelled."
             return True, message
+        elif event['start']['dateTime'] == booking_info['dateTime'] and not booked_event(event, USER_EMAIL):
+            message = "You cannot cancel this. You did not book it."
+            return False, message
 
     message = "No booking to cancel."
     return False, message
-
 
 
 def booked_event(event, USER_EMAIL: str) -> bool:
@@ -210,9 +223,9 @@ def booked_event(event, USER_EMAIL: str) -> bool:
         booked (bool): True is the user is the one who booked the event, otherwise returns False.
     """
     booked = False
-    for attendee in event['attendees']:
-        if attendee['email'] == USER_EMAIL:
-            booked = True
+    attendee = event['attendees'][-1]
+    if attendee['email'] == USER_EMAIL:
+        booked = True
     
     return booked
 
@@ -269,14 +282,14 @@ def get_start_date_time(user_input:str)-> str:
         return "invalid date. Out of range."
 
 
-    if 1<=current_month<=9:
-        current_month = f"0{current_month}" 
-
+    if 1<=booking_month<=9:
+        booking_month = f"0{booking_month}" 
+    
     if 1 <= day <= 9:
         day = f"0{day}"
-
-
-
+    
+    
+    
     return f"2024-{booking_month}-{day}T{time}:00+02:00"
 
 
@@ -297,21 +310,21 @@ def format_time(time : str):
 
         if 8 <= hh <= 9:
             hh = f"0{hh}"
-
+        
         if mm == 0:
             mm = "00"
-
+        
         time = f"{hh}:{mm}"
     else:
         hh = int(time)
 
         if 8 <= hh <= 9:
             hh = f"0{hh}"
-
+        
         time = f"{hh}:00"
-
+    
     return time
-
+     
 
 def time_valid(time : str)->bool:
     """
@@ -346,3 +359,93 @@ def time_valid(time : str)->bool:
     is_time_valid = minute_valid and hour_valid
 
     return is_time_valid      
+
+
+def export_to_ical(creds, ical_file_path):
+    """
+    Exports the bookings in iCal file format.
+
+    Parameters:
+    creds (Credentials): A Credentials object necessary to build a calendar.
+    ical_file_path (str): The location and name of the file you want to export bookings to.
+
+    Returns:
+    message (str): A message stating that the bookings have been exported and also contains the file path to the ics file..
+    """
+    calendar = Calendar()
+    bookings = get_bookings(creds)
+
+    for booking in bookings:
+        event = Event()
+        event.name = booking['summary']
+
+        plus_pos = booking['start']['dateTime'].index("+")
+        start_date_time = booking['start']['dateTime'][:plus_pos]
+        event.begin = dt.datetime.strptime(start_date_time, "%Y-%m-%dT%H:%M:%S")
+
+        plus_pos = booking['end']['dateTime'].index("+")
+        end_date_time = booking['end']['dateTime'][:plus_pos]
+        event.end = dt.datetime.strptime(end_date_time, "%Y-%m-%dT%H:%M:%S")
+        
+        event.description = booking['description']
+        event.location = booking['location']
+
+        event.attendees = []
+        for attendee in booking['attendees']:
+            event.attendees.append(attendee['email'])
+
+        calendar.events.add(event)
+    
+    with open(ical_file_path, "w") as ics_file:
+        ics_file.writelines(calendar)
+    
+    message = f"Bookings exported to {ical_file_path}"
+    return message
+
+
+
+
+
+def get_bookings(creds):
+    bookings = []
+    now = dt.datetime.utcnow().isoformat() + "Z"
+
+    start_date = dt.datetime.utcnow()
+    end_date = dt.datetime.utcnow() + timedelta(days=6)
+
+    # Format dates in RFC3339 format for the API request
+    start_date_str = start_date.isoformat() + 'Z'
+    end_date_str = end_date.isoformat() + 'Z'
+
+    service = build('calendar', 'v3', credentials=creds)
+    events_result = service.events().list(calendarId=CLINIC_CALENDAR_ID, timeMin=now, timeMax=end_date_str, maxResults=150).execute()
+    events = events_result.get("items", [])
+
+    for event in events:
+        if len(event['attendees']) == 2:
+            bookings.append(event)
+
+
+    return bookings
+
+            
+
+
+def is_within_7_days(event, now)-> bool:
+    event_start_date_time = event['start']['dateTime']
+    items = event_start_date_time.split("-")
+    event_year, event_month, event_day = int(items[0]), int(items[1]), int(items[2][:2])
+
+    items = now.split("-")
+    current_year, current_month, current_day = int(items[0]), int(items[1]), int(items[2][:2])
+
+    event_date_time = dt.datetime(event_year, event_month, event_day)
+    now_date_time = dt.datetime(current_year, current_month, current_day)
+
+    delta = event_date_time - now_date_time
+
+    if delta > 6:
+        return False
+    return True
+    
+
